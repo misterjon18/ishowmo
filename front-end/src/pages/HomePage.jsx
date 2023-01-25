628b76be8ae9757e672fa6f3095eb93badb0f44c
misterjon <anthonydeleon135@gmail.com>
2023-01-25 18:22:11 +0800

    Initial HomePage

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <div>
+      create private and public albums, private albums require points to view
+      them additional content requires points to interact with each item
+      advertise relevant collections to all social media outlets to help promote
+      them certain interactions on the platform will give users points that they
+      can use on the platform option to pay by real money or points to unlock
+      premium content
+    </div>
+  );
+}
