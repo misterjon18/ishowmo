26ff20bb21e723804a4008fd6e80fc19288f273d
misterjon <anthonydeleon135@gmail.com>
2023-02-08 18:53:43 +0800

    MAKE A COLLECTION API

--- front-end/src/components/Layout.jsx
+++ front-end/src/components/Layout.jsx
@@ -3,12 +3,20 @@ import Sidebar from "./Sidebar";
 
 export const Layout = () => {
   return (
-    <div style={{ display: "flex", gap: 30 }}>
+    <div
+      style={{
+        display: "flex",
+        gap: 30,
+        height: "100vh",
+        oveflow: "auto",
+        backgroundColor: "green",
+      }}
+    >
       <div style={{ backgroundColor: "pink" }}>
         <Sidebar />
       </div>
 
-      <div>
+      <div style={{ flexGrow: 1 }}>
         <Outlet></Outlet>
       </div>
     </div>
--- front-end/src/components/UserList.jsx
+++ front-end/src/components/UserList.jsx
@@ -1,61 +1,56 @@
 import { useLoaderData } from "react-router-dom";
-import "../styles/UserList.css";
+
+// import "../styles/UserList.css";
 export const UserList = () => {
   const users = useLoaderData();
 
   return (
     <>
-      <div class="container text-center text-white">
-        <div class="row pt-5">
-          <div class="col-lg-8 mx-auto">
-            <h1 class="display-4">Collectors List</h1>
-            <p class="lead mb-0">
-              Build a bootstrap table with a fixed header and scrollable body
-              using Bootstrap 4.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="container py-5">
-        <div class="row">
-          <div class="col-lg-7 mx-auto bg-white rounded shadow">
-            {/* <!-- Fixed header table--> */}
-            <div class="table-responsive">
-              <table class="table table-fixed">
-                {users.map((user, index) => (
-                  <>
-                    <thead key={user.username}>
-                      <tr>
-                        <th scope="col" class="col-3">
-                          #
-                        </th>
-                        <th scope="col" class="col-3">
-                          First
-                        </th>
-                        <th scope="col" class="col-3">
-                          Last
-                        </th>
-                        <th scope="col" class="col-3">
-                          Username
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row" class="col-3">
-                          {index + 1}
-                        </th>
-                        <td class="col-3">{user.first_name}</td>
-                        <td class="col-3">{user.last_name}</td>
-                        <td class="col-3">{user.username}</td>
-                      </tr>
-                    </tbody>
-                  </>
-                ))}
-              </table>
-            </div>
-          </div>
+      <div className="container">
+        <div
+          style={{
+            overflowY: "auto",
+            maxHeight: "200",
+          }}
+        >
+          <table className="table table-fixed">
+            <thead>
+              <tr>
+                <th scope="col" className="col-3">
+                  #
+                </th>
+                <th scope="col" className="col-3">
+                  First
+                </th>
+                <th scope="col" className="col-3">
+                  Last
+                </th>
+                <th scope="col" className="col-3">
+                  Username
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {[
+                ...users,
+                ...users,
+                ...users,
+                ...users,
+                ...users,
+                ...users,
+                ...users,
+              ].map((user, index) => (
+                <tr key={user.username}>
+                  <th scope="row" className="col-3">
+                    {index + 1}
+                  </th>
+                  <td className="col-3">{user.first_name}</td>
+                  <td className="col-3">{user.last_name}</td>
+                  <td className="col-3">{user.username}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
